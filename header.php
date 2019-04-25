5122186a673de5a0eef760d7e3fdc165aad657c0
Eloy <gardenier.eloy30@gmail.com>
2019-04-25 11:48:16 +0200

    overview integrate

--- header.php
+++ header.php
@@ -76,6 +76,9 @@
                                         <li class="nav-item">
                                             <a class="nav-link" href="interaction.php">Interaction</a>
                                         </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="team-overview.php">Team-overview</a>
+                                        </li>
                                     </ul>
                                 </div>
                             </li>
--- pages/team-overview.php
+++ pages/team-overview.php
@@ -1,5 +1,5 @@
 <?php $title = 'Stade Toulousain';
-require 'header.php'; ?>
+require '../header.php'; ?>
 	                <!-- ============================================================== -->
 	                <!-- end pageheader  -->
 	                <!-- ============================================================== -->
@@ -277,22 +277,22 @@ require 'header.php'; ?>
 	                    <!-- ============================================================== -->
 	                    <!-- Optional JavaScript -->
 	                    <!-- jquery 3.3.1 -->
-	                    <script src="assets/vendor/jquery/jquery-3.3.1.min.js"></script>
+	                    <script src="../assets/vendor/jquery/jquery-3.3.1.min.js"></script>
 	                    <!-- bootstap bundle js -->
-	                    <script src="assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
+	                    <script src="../assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
 	                    <!-- slimscroll js -->
-	                    <script src="assets/vendor/slimscroll/jquery.slimscroll.js"></script>
+	                    <script src="../assets/vendor/slimscroll/jquery.slimscroll.js"></script>
 	                    <!-- main js -->
-	                    <script src="assets/libs/js/main-js.js"></script>
+	                    <script src="../assets/libs/js/main-js.js"></script>
 	                    <!-- morris-chart js -->
-	                    <script src="assets/vendor/charts/morris-bundle/raphael.min.js"></script>
-	                    <script src="assets/vendor/charts/morris-bundle/morris.js"></script>
-	                    <script src="assets/vendor/charts/morris-bundle/morrisjs.html"></script>
+	                    <script src="../assets/vendor/charts/morris-bundle/raphael.min.js"></script>
+	                    <script src="../assets/vendor/charts/morris-bundle/morris.js"></script>
+	                    <script src="../assets/vendor/charts/morris-bundle/morrisjs.html"></script>
 	                    <!-- chart js -->
-	                    <script src="assets/vendor/charts/charts-bundle/Chart.bundle.js"></script>
-	                    <script src="assets/vendor/charts/charts-bundle/chartjs.js"></script>
+	                    <script src="../assets/vendor/charts/charts-bundle/Chart.bundle.js"></script>
+	                    <script src="../assets/vendor/charts/charts-bundle/chartjs.js"></script>
 	                    <!-- dashboard js -->
-	                    <script src="assets/libs/js/dashboard-influencer.js"></script>
+	                    <script src="../assets/libs/js/dashboard-influencer.js"></script>
 </body>
  
 </html>
